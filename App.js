f0b00552f2f960277b7943d0ef141ea846d84cb0
AnatolyKazarin <alkidkaz@gmail.com>
2020-09-21 02:44:44 +0300

    5 Screens created

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AppLoading } from "expo";
+import { bootstrap } from "./src/bootstrap";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={bootstrap}
+        onFinish={() => {
+          setIsReady(true);
+        }}
+        onError={(e) => console.log(e)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -10,12 +26,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
